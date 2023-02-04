5e70a3599f7458cc9e4a3f0b59b276486e59fda2
Simon Foster <funkysi1701@gmail.com>
2023-02-04 09:57:59 +0000

    Initial Commit of MAUI app

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    public void TapGestureRecognizer_Tapped(object sender, TappedEventArgs args)
+    {
+        var url = (string)args.Parameter;
+        Launcher.OpenAsync(new Uri(url));
+    }
+}
---
+++ MauiApp1/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace MauiApp1
+{
+    public class MainViewModel
+    {
+    }
+}
