5780b8f17ac36e7377e20d0ff638113552982071
Simon Foster <funkysi1701@gmail.com>
2023-02-04 10:45:20 +0000

    rename

--- MauiApp1/MainViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MauiApp1
-{
-    public class MainViewModel
-    {
-    }
-}
---
+++ PwnedPass/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PwnedPass;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
